fa90a477b9a818839f52f5351ec870a1831d75d2
Tri Songz <tri@songzcorp.com>
2020-04-14 15:11:06 -0500

    fx

--- generator/simple/finetune.py
+++ generator/simple/finetune.py
@@ -3,20 +3,20 @@ import tarfile
 
 import gpt_2_simple as gpt2
 
-model_name = "1558M"
+model_name = "3458M"
 if not os.path.isdir(os.path.join("models", model_name)):
     print("Downloading ", model_name, " model...")
     gpt2.download_gpt2(
         model_name=model_name
     )  # model is saved into current directory under /models/124M/
 
-file_name = "text_adventures.txt"
+file_name = "/content/text.txt"
 
 sess = gpt2.start_tf_sess()
 gpt2.finetune(
     sess,
     file_name,
-    multi_gpu=True,
+    multi_gpu=False,
     batch_size=32,
     learning_rate=0.0001,
     model_name=model_name,
